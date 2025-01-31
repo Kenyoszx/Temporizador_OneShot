9d418672bad5d2cdbd622e741b8fa69443dbed54
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-31 11:41:00 -0300

    Definição e inicialização pinos

--- OneShot.c
+++ OneShot.c
@@ -2,23 +2,50 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
-}
+#define LED_PIN_GREEN 11
+#define LED_PIN_BLUE 12
+#define LED_PIN_RED 13
+#define BUTTON_A_PIN 5
 
+bool led_active = false;
+
+void init();
+int64_t alarm_callback(alarm_id_t id, void *user_data);
 
 
 int main()
 {
     stdio_init_all();
+    init();
+    add_alarm_in_ms(3000, alarm_callback, NULL, false);
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    while (true)
+    {
+        
     }
+}
+
+void init()
+{
+    gpio_init(LED_PIN_GREEN);
+    gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
+    gpio_put(LED_PIN_GREEN, 0);
+
+    gpio_init(LED_PIN_BLUE);
+    gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
+    gpio_put(LED_PIN_BLUE, 0);
+
+    gpio_init(LED_PIN_RED);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_put(LED_PIN_RED, 0);
+
+    // inicialização do botões com resistor interno pull-up
+    gpio_init(BUTTON_A_PIN);
+    gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_A_PIN);
+}
+int64_t alarm_callback(alarm_id_t id, void *user_data)
+{
+
+    return 0;
 }
